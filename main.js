f791e7c3f06ad2b9c26b971074e30043ca6f24b3
Daniel <dbo@ua.pt>
2018-06-23 16:33:10 +0100

    fixed last commit

--- main.js
+++ main.js
@@ -21,15 +21,15 @@ var soldiersNum = 0, soldierCost = 15, soldierBaseCost = 15, priestsNum = 0, pri
     function update(){
         click.onclick = function() {gold = gold + 1;};
 		
-        Military0.onclick = function() {
+        /*Military0.onclick = function() {
         var i = 0;
         if (gold >= militaryCost[i]) {
             gold = gold - militaryCost[i];
             militaryNum[i]++;
-            militaryGPS[i] = militaryBaseGPS[i] * militaryNum[i] * militaryMultiplier[0]/* + militaryAdder[0]*/;
+            militaryGPS[i] = militaryBaseGPS[i] * militaryNum[i] * militaryMultiplier[0];// + militaryAdder[0];
             militaryCost[i] = militaryBaseCost[i] * (Math.pow(1.3, militaryNum[i]));
             };
-        };
+        };*/
 	Priests.onclick = function() {
         if (gold >= priestCost) {
             gold = gold - priestCost;
@@ -59,14 +59,45 @@ var soldiersNum = 0, soldierCost = 15, soldierBaseCost = 15, priestsNum = 0, pri
         document.getElementById("Gold").innerHTML  = "Gold: ".bold() + gold.toFixed(0) + " (per second: " + militaryGPS.reduce((a, b) => a + b, 0).toFixed(1) + ")"; //TODO: temporary (fast) solution, it's too intesive TODO: needs global gps
 	document.getElementById("Faith").innerHTML  = "Faith: ".bold() + faith.toFixed(0) + " (per second: " + fps.toFixed(0) + ")";
 	document.getElementById("Mana").innerHTML  = "Mana: ".bold() + mana.toFixed(0) + " (per second: " + mps.toFixed(0) + ")";
-		
+	
+	//Military display	
         Military0.value = militaryCost[0].toFixed(0) + " Gold";
         MilitaryNum0.value = militaryNum[0].toFixed(0);
-		Priests.value = priestCost.toFixed(0) + " Gold";
+        Military1.value = militaryCost[1].toFixed(0) + " Gold";
+        MilitaryNum1.value = militaryNum[1].toFixed(0);
+        Military2.value = militaryCost[2].toFixed(0) + " Gold";
+        MilitaryNum2.value = militaryNum[2].toFixed(0);
+        Military3.value = militaryCost[3].toFixed(0) + " Gold";
+        MilitaryNum3.value = militaryNum[3].toFixed(0);
+        Military4.value = militaryCost[4].toFixed(0) + " Gold";
+        MilitaryNum4.value = militaryNum[4].toFixed(0);
+        Military5.value = militaryCost[5].toFixed(0) + " Gold";
+        MilitaryNum5.value = militaryNum[5].toFixed(0);
+        Military6.value = militaryCost[6].toFixed(0) + " Gold";
+        MilitaryNum6.value = militaryNum[6].toFixed(0);
+        Military7.value = militaryCost[7].toFixed(0) + " Gold";
+        MilitaryNum7.value = militaryNum[7].toFixed(0);
+        
+	Priests.value = priestCost.toFixed(0) + " Gold";
         PriestsNum.value = priestsNum.toFixed(0);
-		Sorcerers.value = sorcererCost.toFixed(0) + " Gold";
+	Sorcerers.value = sorcererCost.toFixed(0) + " Gold";
         SorcerersNum.value = sorcerersNum.toFixed(0);
     };
     //function updateGPS()
+    function militaryBuyClick(i) {
+    	if (gold >= militaryCost[i]) {
+            gold = gold - militaryCost[i];
+            militaryNum[i]++;
+            militaryGPS[i] = militaryBaseGPS[i] * militaryNum[i] * militaryMultiplier[0]/* + militaryAdder[0]*/;
+            militaryCost[i] = militaryBaseCost[i] * (Math.pow(1.3, militaryNum[i]));
+	}
+    }
+    
+    function giveGoldFaithMana(g, f, m) {
+    	gold += g;
+    	faith += f;
+    	mana += m;
+    }
+    
     var mainloop = function() {update(), draw()}; 
     setInterval(mainloop, 16);
